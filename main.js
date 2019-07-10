d87f2cd7efe4861752283ad798a8dbc48f14c415
Mirco Wittrien <mirco.wittrien@gmail.com>
2019-07-10 12:56:02 +0200

    yes

---
+++ main.js
@@ -0,0 +1,128 @@
+﻿window.addEventListener("load", function () {
+	console.log("Verbunden.");
+	var socket = io();
+	var login = null;
+	var settings_cog = document.getElementById("settings_cog");
+	var settings_menu = document.getElementById("settings_menu");
+	var login_controls = document.getElementById("login_controls");
+	var login_input = document.getElementById("login_input");
+	var login_btn = document.getElementById("login_btn");
+	var logout_controls = document.getElementById("logout_controls");
+	var logout_btn = document.getElementById("logout_btn");
+	var last_entry = document.getElementById("last_entry");
+	var advanced_controls = document.getElementById("advanced_controls");
+	var undo_btn = document.getElementById("undo_btn");
+	var redo_btn = document.getElementById("redo_btn");
+	var reset_btn = document.getElementById("reset_btn");
+	var numbertiles = document.getElementsByClassName("number tile");
+	settings_cog.addEventListener("click", function (e) {
+		settings_menu.classList.toggle("closing");
+	});
+	login_btn.addEventListener("click", function (e) {
+		if (!login && login_input.value) socket.emit("loginReq", {pw:login_input.value});
+	});
+	logout_btn.addEventListener("click", function (e) {
+		if (login) socket.emit("logoutReq", {login});
+	});
+	undo_btn.addEventListener("click", function (e) {
+		if (login) socket.emit("bingoUndo", {login});
+	});
+	redo_btn.addEventListener("click", function (e) {
+		if (login) socket.emit("bingoRedo", {login});
+	});
+	reset_btn.addEventListener("click", function (e) {
+		if (login) socket.emit("bingoReset", {login});
+	});
+	for (let numbertile of numbertiles) numbertile.addEventListener("click", function (e) {
+		if (login) socket.emit("bingoData", {login, number:numbertile.getAttribute("number"), state:!numbertile.classList.contains("on")});
+	});
+	socket.on("inOutAuth", function (data) {
+		login_input.value = "";
+		if (data.login) {
+			login = data.login;
+			login_controls.style.setProperty("display", "none");
+			advanced_controls.style.removeProperty("display");
+			logout_controls.style.removeProperty("display");
+		}
+		else {
+			login = null;
+			login_controls.style.removeProperty("display");
+			advanced_controls.style.setProperty("display", "none");
+			logout_controls.style.setProperty("display", "none");
+		}
+		settings_cog.click();
+	});
+	socket.on("showToast", function (data) {
+		console.log(data.msg);
+		toast(data.msg, {type:data.type});
+	});
+	socket.on("bingoUpdate", function (data) {
+		for (let numbertile of numbertiles) {
+			var number = numbertile.getAttribute("number");
+			if (number) numbertile.classList.toggle("on", data.numbers.includes(number));
+		}
+		if (data.undo.length > 0) undo_btn.removeAttribute("disabled");
+		else undo_btn.setAttribute("disabled", "");
+		if (data.redo.length > 0) redo_btn.removeAttribute("disabled");
+		else redo_btn.setAttribute("disabled", "");
+		if (data.numbers.length > 0) reset_btn.removeAttribute("disabled");
+		else reset_btn.setAttribute("disabled", "");
+		var lastnumber = data.numbers.length > 0 ? data.numbers[data.numbers.length-1] : null;
+		if (lastnumber) {
+			var lastnumbertile = document.querySelector(`.tile.number[number="${lastnumber}"]`);
+			if (lastnumbertile) return last_entry.textContent = lastnumbertile.parentElement.getAttribute("column") + " " + lastnumber;
+		}
+		last_entry.textContent = "---";
+	});
+	var forcevideo = htmlToElement('<video controls muted playsinline autoplay src="data:video/webm;base64,GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA=" style="width: 0 !important; height: 0 !important; opacity: 0 !important; pointer-events: none !important;"></video>');
+	forcevideo.addEventListener("loadedmetadata", () => {
+		if (forcevideo.duration <= 1) forcevideo.setAttribute("loop", "");
+		else forcevideo.addEventListener("timeupdate", () => {
+			if (forcevideo.currentTime > 0.5) forcevideo.currentTime = Math.random();
+		});
+	});
+	document.getElementById("settings_menu").append(forcevideo);
+});
+var htmlToElement = function (html) {
+	if (!html || !html.trim()) return null;
+	let template = document.createElement("template");
+	template.innerHTML = html.replace(/\t|\n|\r/g, "");
+	if (template.content.childElementCount == 1) return template.content.firstElementChild;
+	else {
+		var wrapper = document.createElement("span");
+		var nodes = Array.from(template.content.childNodes);
+		while (nodes.length) wrapper.appendChild(nodes.shift());
+		return wrapper;
+	}
+};
+var encodeToHTML = function (string) {
+	var ele = document.createElement("div");
+	ele.innerText = string;
+	return ele.innerHTML;
+};
+var toast = function (text, options = {}) {
+	var toasts = document.querySelector(".toasts");
+	if (!toasts) {
+		toasts = htmlToElement(`<div class="toasts"></div>`);
+		document.body.appendChild(toasts);
+	}
+	const {type = '', icon = true, timeout = 3000, html = false} = options;
+	var toast = htmlToElement(`<div class="toast">${html === true ? text : encodeToHTML(text)}</div>`);
+	if (type) {
+		toast.classList.add(`toast-${type}`);
+		if (icon) toast.classList.add("toast-icon");
+	}
+	toasts.appendChild(toast);
+	toast.close = () => {
+		if (document.contains(toast)) {
+			toast.classList.add("closing");
+			setTimeout(() => {
+				toast.remove();
+				if (!toasts.querySelectorAll(".toast").length) toasts.remove();
+			}, 3000);
+		}
+	};
+	toast.addEventListener("click", toast.close);
+	setTimeout(() => {toast.close();}, timeout > 0 ? timeout : 600000);
+	return toast;
+};
---
+++ webserver.js
@@ -0,0 +1,123 @@
+﻿const express = require("express");
+const app = express();
+
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+const fs = require("fs");
+const path = require("path");
+
+http.listen(8080); //listen to port 8080
+
+app.get("/", function (req, res) {
+	res.sendFile(path.resolve(__dirname, "./public/index.html"));
+	app.use("/imgs", express.static(path.resolve(__dirname, "./public/imgs")));
+	app.use("/css", express.static(path.resolve(__dirname, "./public/css")));
+	app.use("/js", express.static(path.resolve(__dirname, "./public/js")));
+});
+
+var sockets = [], numbers = [], undo = [], redo = [];
+function addEntry (array, entry) {
+	if (array.indexOf(entry) == -1) {
+		array.push(entry);
+		return true;
+	}
+	return false;
+}
+function removeEntry (array, entry) {
+	if (array.indexOf(entry) > -1) {
+		array.splice(array.indexOf(entry), 1);
+		return true;
+	}
+	return false;
+}
+function sendUpdate () {
+	for (let connected of sockets) connected.emit("bingoUpdate", {numbers,undo,redo});
+}
+function sendPerError (socket) {
+	socket.emit("showToast", {msg:"Bitte einloggen um die Schalter zu betätigen.", type:"error"})
+}
+function btoa (string) {
+	if (typeof string != "string" && typeof string != "number") return "";
+	return Buffer.from(string.toString()).toString("base64");
+}
+io.sockets.on("connection", function (socket) {
+	addEntry(sockets, socket);
+	console.log(`Neue Verbindung. (${sockets.length} verbunden)`);
+	var login = null;
+	socket.emit("bingoUpdate", {numbers, undo, redo});
+	socket.on("loginReq", function (data) {
+		if (login) socket.emit("showToast", {msg:"Bereits eingeloggt."});
+		else if (data.pw == "friendsbingo123") {
+			login = btoa(Math.random()*10000) + btoa(Math.random()*10000);
+			socket.emit("inOutAuth", {login, footer:"", buttons:""});
+			socket.emit("showToast", {msg:"Login erfolgreich.", type:"success"});
+		}
+		else socket.emit("showToast", {msg:"Login fehlgeschlagen.", type:"error"});
+	});
+	socket.on("logoutReq", function (data) {
+		if (!login) socket.emit("showToast", {msg:"Bereits ausgeloggt."});
+		else if (login && data.login == login) {
+			login = null;
+			socket.emit("inOutAuth", {});
+			socket.emit("showToast", {msg:"Logout erfolgreich.", type:"success"});
+		}
+		else socket.emit("showToast", {msg:"Logout fehlgeschlagen.", type:"error"});
+	});
+	socket.on("bingoData", function (data) {
+		if (!login) sendPerError(socket);
+		else if (login && data.login == login && data.number) {
+			var undonumbers = [].concat(numbers);
+			if (data.state) {
+				if (addEntry(numbers, data.number)) {
+					undo.push(undonumbers);
+					redo = [];
+					sendUpdate();
+				}
+			}
+			else if (!data.state) {
+				if (removeEntry(numbers, data.number)) {
+					undo.push(undonumbers);
+					redo = [];
+					sendUpdate();
+				}
+			}
+		}
+	});
+	socket.on("bingoUndo", function (data) {
+		if (!login) sendPerError(socket);
+		else if (login && data.login == login && undo.length > 0) {
+			redo.unshift([].concat(numbers));
+			numbers = undo.pop();
+			sendUpdate();
+		}
+	});
+	socket.on("bingoRedo", function (data) {
+		if (!login) sendPerError(socket);
+		else if (login && data.login == login && redo.length > 0) {
+			undo.push([].concat(numbers));
+			numbers = redo.shift();
+			sendUpdate();
+		}
+	});
+	socket.on("bingoReset", function (data) {
+		if (!login) sendPerError(socket);
+		else if (login && data.login == login && numbers.length > 0) {
+			undo.push([].concat(numbers));
+			numbers = [];
+			redo = [];
+			sendUpdate();
+		}
+	});
+	socket.on("disconnect", function (data) {
+		removeEntry(sockets, socket);
+		console.log(`Verbindung getrennt. (${sockets.length} verbunden)`);
+		login = null;
+	});
+});
+
+process.on("SIGINT", function () { //on ctrl+c
+	process.exit(); //exit completely
+});
+
+console.log("Bingo Node Server gestartet.");
